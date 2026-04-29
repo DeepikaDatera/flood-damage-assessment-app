14b50c907f2150aed3fed2e356f2bf169552293f
DeepikaDatera <your-new-email@example.com>
2026-04-29 18:42:19 +0530

    Updated port in backend

--- flood-backend/index.js
+++ flood-backend/index.js
@@ -14,10 +14,11 @@ server.use(express.json())
 server.use('/uploads', express.static('uploads'));
 server.use('/api/assessment', assessmentRoutes)
 
+const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Connected to MongoDB')
-    server.listen(3000, () => {
-        console.log('Server is running on port 3000')
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
     })
 }).catch((err) => {
     console.error('Error connecting to MongoDB:', err)
